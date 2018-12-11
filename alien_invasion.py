d8f7f4e3ec569b5804e39bcc5327c3f819d6e657
sweetpotato <justsweetpotato@gmail.com>
2018-12-11 17:11:26 +0800

    游戏背景和飞船创建

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # 初始化并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+        # 响应按键和鼠标事件
+        gf.check_events()
+
+        # 绘制屏幕
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class Settings():
+    def __init__(self):
+        '''初始化游戏设置'''
+        # 屏幕设置
+        self.screen_width = 1100
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
