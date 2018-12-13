92e8ecd389a37ca72ab530a0e4915eaeaa888b85
sweetpotato <justsweetpotato@gmail.com>
2018-12-13 16:19:30 +0800

    创建外星人实例

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
-import sys
 import pygame
 from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -24,13 +23,16 @@ def run_game():
     # 创建一个用于存储子弹的编组
     bullets = Group()
 
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
+
     # 开始游戏的主循环
     while True:
         # 响应按键和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
