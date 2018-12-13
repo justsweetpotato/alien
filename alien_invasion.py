760363d054057c7256efa919d4a3d02def2bb583
sweetpotato <justsweetpotato@gmail.com>
2018-12-13 15:49:32 +0800

    发射子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@
 
 import sys
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -20,16 +21,22 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 开始游戏的主循环
     while True:
         # 响应按键和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
 
         # 飞船移动
         ship.update()
 
+        # 子弹移动
+        bullets.update()
+
         # 绘制屏幕
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
