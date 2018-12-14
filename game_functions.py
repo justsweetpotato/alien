e22a3d3c4e9d2c6c7fdbe03d3a505a950d07498d
sweetpotato <justsweetpotato@gmail.com>
2018-12-14 17:30:04 +0800

    修复paly按钮不可见时仍然起作用的问题

--- game_functions.py
+++ game_functions.py
@@ -52,7 +52,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     '''玩家单机play按钮开始游戏'''
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # 重置统计信息
         stats.reset_stats()
         stats.game_active = True
