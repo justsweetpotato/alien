89c7ce816f41eb74ee5ad2393f3abf6fdc65fceb
sweetpotato <justsweetpotato@gmail.com>
2018-12-14 18:04:03 +0800

    显示计分板

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 def run_game():
@@ -21,8 +22,10 @@ def run_game():
     # 创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
 
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建一个用于存储游戏统计信息的实例和计分板
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
 
     # 创建一艘飞船, 一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
@@ -39,7 +42,7 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
