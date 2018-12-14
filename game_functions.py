7238804ec2fd2b7a12c22e9aaaa898d3b5f57e30
sweetpotato <justsweetpotato@gmail.com>
2018-12-14 16:57:01 +0800

    失败三次游戏结束

--- game_functions.py
+++ game_functions.py
@@ -144,18 +144,22 @@ def change_fleet_direction(ai_settings, aliens):
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     '''响应被外星人撞到的飞船'''
     # 将ships_left -1
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
 
-    # 清空外星人和子弹
-    aliens.empty()
-    bullets.empty()
+        # 清空外星人和子弹
+        aliens.empty()
+        bullets.empty()
 
-    # 创建一群新外星人, 重生飞船
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # 创建一群新外星人, 重生飞船
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    # 暂停
-    sleep(0.5)
+        # 暂停
+        sleep(0.5)
+
+    else:
+        stats.game_active = False
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
